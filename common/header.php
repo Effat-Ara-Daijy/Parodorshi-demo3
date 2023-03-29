6864b4beccf8905faa65a60ecc83174dda996a7b
Effat-Ara-Daijy <daijyeffat@gmail.com>
2023-03-29 13:54:45 +0600

    push

--- common/header.php
+++ common/header.php
@@ -47,44 +47,6 @@
                                     <li class="nav-item english">
                                         <a class="nav-link en-btn" href="#">English</a>
                                     </li>
-                                    <!-- <li class="nav-item">
-                                        <a class="nav-link" aria-current="page" href="#">আমাদের সম্পর্কে</a>
-                                    </li>
-                                    <li class="dropdown nav-item">
-                                        <a class="nav-link" href="#" data-bs-toggle="dropdown">প্রশিক্ষণ ও শিক্ষা
-                                            <i class="fas fa-chevron-down fa-xs"></i></a>
-                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                            <li><a class="dropdown-item" href="#">টেইলরিং অ্যান্ড ড্রেস মেকিং</a></li>
-                                            <li><a class="dropdown-item" href="#">কম্পিউটার অপারেটর</a></li>
-                                            <li><a class="dropdown-item" href="#">গ্রাফিক ডিজাইন</a></li>
-                                            <li><a class="dropdown-item" href="#">ইলেকট্রিক্যাল ইন্সটলেশন অ্যান্ড
-                                                    মেইনটেনেন্স</a></li>
-                                            <li><a class="dropdown-item" href="#">ম্যাসনারী</a></li>
-                                            <li><a class="dropdown-item" href="#">রড বাইন্ডিং</a></li>
-                                            <li><a class="dropdown-item" href="#">প্লাম্বিং অ্যান্ড পাইপ ফিটিং</a></li>
-                                            <li><a class="dropdown-item" href="#">টাইলস অ্যান্ড মার্বেল ফিটিং</a></li>
-                                            <li><a class="dropdown-item" href="#">মোটর সাইকেল রিপেয়ারিং অ্যান্ড
-                                                    সার্ভিসিং</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">অনলাইন ভর্তি</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">রেজাল্ট</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">উৎপাদন ও সেবা</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">কর্মজীবন</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">যোগাযোগ</a>
-                                    </li>
-                                    <li class="nav-item english">
-                                        <a class="nav-link en-btn" href="#">English</a>
-                                    </li> -->
                                 </ul>
                             </div>
                         </div>
